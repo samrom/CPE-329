34d263f8903dd8abfd26dcd76c7676309c006e01
samrom <sam.romano18@gmail.com>
2016-04-06 22:09:29 -0700

    Update main.c
    Nibble mode working and says hello world!

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 
  void lcd_write_address(char address);
 
+ void lcd_initialization();
+
 /*
 
  * main.c
@@ -43,13 +45,13 @@ int main(void) {
 
 	DCOCTL = 0;
 
-			  BCSCTL1 = CALBC1_16MHZ;
+	  BCSCTL1 = CALBC1_16MHZ;
 
-			  DCOCTL = CALDCO_16MHZ;
+	  DCOCTL = CALDCO_16MHZ;
 
 //MSP430 Pins
 
-    P1DIR = 0x0F; // sets port 1 pins as an output
+    P1DIR = 0xF0; // sets port 1 pins as an output
 
     P2DIR = 0x07; // sets E, RW, and RS as output 0000 0111
 
@@ -57,7 +59,8 @@ int main(void) {
     lcd_initialization();
     lcd_write_address(0x00);  // display set 0000 1111
     lcd_write_data('H');
-    lcd_write_address( 0x01);
+    _delay_cycles(4);
+    lcd_write_address(0x01);
     lcd_write_data('E');
 	lcd_write_address( 0x02);
 	lcd_write_data('L');
@@ -81,7 +84,7 @@ int main(void) {
 	lcd_write_data('D');
 	lcd_write_address( 0x0B);
 	lcd_write_data('!');
-	
+//
 	return 0;
 
 }
@@ -93,62 +96,73 @@ int main(void) {
 
  */
 void lcd_initialization(){
-    _delay_cycles(640000); // 40 ms delay
-    P2OUT &= ~0x07;
+    _delay_cycles(6400000); // 40 ms delay
     P2OUT |= 0x01; // set enable high
-    P1OUT = 0x50;
+    P1OUT = BIT5;  // nibble mode
+    P2OUT &= ~0x01; // toggle enable off.
+    P1OUT &= ~BIT5;
+
+    P2OUT &= ~0x07;
+    _delay_cycles(16);
     lcd_write_cmd(0x2F);
-    _delay_cycles(700); // 39+ us delay
+    _delay_cycles(7200); // 39+ us delay
     lcd_write_cmd(0x0F);  // display set 0000 1111
-    _delay_cycles(700);   // 37 us delay
+    _delay_cycles(7200);   // 37 us delay
     lcd_write_cmd(0x01);  // display clear
-    _delay_cycles(30000); // 1.53 + extra ms
-    lcd_write_cmd(0x04);
-    _delay_cycles(30000); // 1.52ms
+    _delay_cycles(34000); // 1.53 + extra ms
+    lcd_write_cmd(0x06);
+    _delay_cycles(34000); // 1.52ms
+
+    P2OUT ^= 0x01; // set enable low.
 }
 void lcd_write_cmd(char cmd){
 
-	P2OUT &= ~0x07;
+	//P2OUT &= ~0x07;
 	P2OUT |= 0x01; // set enable high
-
-	// Top bit
-	P1OUT = (cmd << 4) & (BIT7 + BIT6 + BIT5 + BIT4);
-	_delay_cycles(4);
-	P2OUT &= ~0x07; // reset
-	P2OUT |= 0x01; // set enable high again
-
-	//Bottom Bit
-	P1OUT = cmd & (BIT3 + BIT2 + BIT1 + BIT0);
-	_delay_cycles(4); // delay 1us.
+	//Top Bit
+	P1OUT = cmd & (BIT7 | BIT6 | BIT5 | BIT4);
+	_delay_cycles(16); // delay 1us.
+	P2OUT ^= 0x01; // set enable off
+	_delay_cycles(16);
+    P2OUT ^= 0x01; // enable high
+	// bottom bit
+	P1OUT = (cmd << 4)  & (BIT7 | BIT6 | BIT5 | BIT4);
+	_delay_cycles(16);
+	P2OUT ^= 0x01; // enable low
 
 	//Reset
 	P2OUT &= ~0x07;
 	P1OUT = 0x00;
-	_delay_cycles(592); // 37 us delay
+	_delay_cycles(7200); // 37 us delay
     return;
 
 }
 
 void lcd_write_data(char data){
     //Setup
-	P2OUT &= ~0x07; //clear BIT0, BIT1, BIT2
-	P2OUT |= 0x04; // set RS high
-	_delay_cycles(0.64); // 40ns delay
-	P2OUT |= 0x01; // set enable high
+	//P2OUT &= ~0x07; //clear BIT0, BIT1, BIT2
+	P2OUT |= BIT2; // set RS high
 
-	// Top bit
-	P1OUT = (data << 4) & (BIT7 + BIT6 + BIT5 + BIT4);
-	_delay_cycles(4);
-
-	//Setup again.
-	P2OUT &= ~0x07; //clear BIT0, BIT1, BIT2
-	P2OUT |= 0x04; // set RS high
-	_delay_cycles(0.64); // 40ns delay
+	//_delay_cycles(0.64); // 40ns delay
 	P2OUT |= 0x01; // set enable high
 
 	//Bottom Bit
-	P1OUT = cmd & (BIT3 + BIT2 + BIT1 + BIT0);
+	P1OUT =  data & (BIT7 | BIT6 | BIT5 | BIT4);
 	_delay_cycles(4); // delay 1us.
+
+	P2OUT ^= 0x01; // set enable off
+	//P2OUT ^= BIT2; // set RS low
+//
+
+	_delay_cycles(16);
+	P2OUT ^= 0x01; // set enable high
+
+	// Top bit
+	P1OUT = (data<<4) & (BIT7 | BIT6 | BIT5 | BIT4);
+	_delay_cycles(4);
+	P2OUT ^= 0x01; // set enable off
+	//P2OUT ^= BIT2;
+
 
 	//Clear everything again.
 	P2OUT &= ~0x07; // sets everything low
@@ -158,7 +172,8 @@ void lcd_write_data(char data){
 }
 
 void lcd_write_address(char address){
-	 lcd_write_cmd(BIT7 + address);  // address on the LCD
-	 _delay_cycles(592); // 37 us delay
+	// P1OUT = 0x00;
+	 lcd_write_cmd(address | 0x80);  // address on the LCD
+	 _delay_cycles(800); // 37+ us delay
 	return;
 }
