d02f3a4c8dd30fdf1a60884ae7bf18efc3875359
erodri66 <edi.rodrigz6@gmail.com>
2016-04-14 19:51:11 -0700

    Update main.c
    ACLK OPERATING AT 32.7 KZ and led on for a sec

--- Assingment5/main.c
+++ Assingment5/main.c
@@ -2,39 +2,41 @@
 
 /*
  * main.c
+ * This is Assingment 5 using crystal oscillator
  */
-int i=0;
-int main(void) {
-
-    //16Mhz
-    if (CALBC1_16MHZ==0xFF)
-    {
-    	while(1);
-	}
-	DCOCTL = 0;
-	BCSCTL1 = CALBC1_16MHZ;
-	DCOCTL = CALDCO_16MHZ;
-
-	WDTCTL = WDTPW + WDTHOLD;	// Stop watchdog timers
-
-	BCSCTL1 |= DIVA_0;
-	P1DIR |= 0X01;
-	CCTL0 = CCIE;
-	CCR0=32767;
-	TACTL = TASSEL_1 + ID_0 + MC_1;
-	 _BIS_SR(LPM0_bits + GIE);
 
 
-	
-	//return 0;
-}
-// Timer A0 interrupt service routine
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer_A (void)
-{
-	P1SEL |= BIT0;
-	P1SEL2 &= ~BIT0;
+#define cycles 32768
 
-    CCR0 += 32767 ;
-}
+ void main(void)
+ {
 
+	WDTCTL = WDTPW + WDTHOLD;			// Stop WDT
+
+	//P1DIR |= BIT6;
+
+ 	P1DIR |= BIT0 +BIT6;					// set P1.0 (LED1) as output
+ 	P1SEL |= BIT0;
+ 	P1SEL2 &= ~BIT0;
+
+ 	BCSCTL3 |= XCAP_3;
+
+ 	CCTL0 = CCIE;					// CCR0 interrupt enabled
+ 	CCR0 = cycles;					// 512 -> 1 sec, 30720 -> 1 min
+ 	TACTL = TASSEL_1 + MC_2;			// ACLK, cONTMODE
+
+
+ 	_BIS_SR(LPM0_bits + GIE);			// Enter LPM3 w/ interrupt ?? LPM0 or LMP3?
+
+ }
+
+ // Timer A0 interrupt service routine
+ #pragma vector=TIMER0_A0_VECTOR
+ __interrupt void Timer_A (void)
+ {
+
+	P1OUT ^= BIT6;
+	CCR0 += cycles;
+
+
+ }
